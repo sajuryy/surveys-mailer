b3d1a717c6f4ea245cc9f30daa3821d151d716e6
sajuryy <sajuryy@gmail.com>
2018-04-17 20:21:25 +0300

    heroku

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
+const passport = require('passport');
 const app = express();
 const  port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-    res.send({
-        hi: 'there'
-    });
-});
+
 
 app.listen(port, console.log(`server running on ${port}`));

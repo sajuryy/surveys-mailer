a6068a9ca38baa95e4a68a3aa906c3755d0465ec
sajuryy <sajuryy@gmail.com>
2018-04-25 20:37:46 +0300

    client side login, logout, /, dashboard

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async dispatch => {
+export const fetchUser = () => async dissspatch => {
     const res = await axios.get('/api/current_user');
   
-    dispatch({ type: FETCH_USER, payload: res });
+    dissspatch({ type: FETCH_USER, payload: res.data });
   };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null: return;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="/" className="left brand-logo">FSWD</a>
+
+                    <Link 
+                    to={this.props.auth ? '/surveys' : '/'} 
+                    className="left brand-logo"
+                    >
+                    FSWD
+                    </Link>
+
                     <ul className="right">
-                        <li><a>Login with Google</a></li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -15,4 +33,8 @@ class Header extends Component {
     };
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);

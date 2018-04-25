4455b78720d0151635c8fe699efe9d9d54cb6035
sajuryy <sajuryy@gmail.com>
2018-04-25 19:12:15 +0300

    haos

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,20 @@
+// export default function (state = {}, action) {
+//     console.log(action);
+//     switch (action.type) {
+//         default:
+//             return state;
+//     }
+// }
+
+
+// import { FETCH_USER } from '../actions/types';
+
+export default function(state = {}, action) {
+    console.log(action);
+  switch (action.type) {
+    // case FETCH_USER:
+    //   return action.payload || false;
+    default:
+      return state;
+  }
+}

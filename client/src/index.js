f3ce7bf266bd2c576c6367bc84f6d40bc16aa976
sajuryy <sajuryy@gmail.com>
2018-04-26 23:18:41 +0300

    stripe key

--- client/src/index.js
+++ client/src/index.js
@@ -18,3 +18,6 @@ ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector('#root')
 );
+
+console.log(process.env.REACT_APP_STRIPE_KEY);
+console.log(process.env.NODE_ENV);

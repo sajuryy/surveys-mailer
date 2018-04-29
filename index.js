66766bcf587b884430c412983ff5ca03b1d997a8
sajuryy <sajuryy@gmail.com>
2018-04-29 19:42:40 +0300

    deployment configs

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(('client/build')));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 mongoose.connect(keys.mongoURI);
 
 const port = process.env.PORT || 8080;

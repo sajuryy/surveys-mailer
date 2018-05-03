7da1c3319c6512f4c7f33db9bf387ac4e21d7a5e
sajuryy <sajuryy@gmail.com>
2018-05-03 20:33:10 +0300

    survey post, thx

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,22 +1,42 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+    app.get('/api/surveys/thx', (req, res) => {
+        res.send('THX');
+    });
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email => ({ email })),
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
             _user: req.user.id,
             dateSent: Date.now()
-        })
+        });
 
-        survey.save();
+        // Great place to send an email!
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
+
 };

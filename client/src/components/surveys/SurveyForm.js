02daea043a1d804fa261ee1af8c96b9f2d3f46f3
sajuryy <sajuryy@gmail.com>
2018-05-12 00:05:37 +0300

    survey submit

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -39,7 +39,7 @@ class SurveyFrom extends Component {
 function validate(values) {
     const errors = {};
 
-    errors.emails = validateEmails(values.emails || '');
+    errors.recipients = validateEmails(values.recipients || '');
 
     _.each(formFields, ({ name }) => {
         if (!values[name]) {
--- client/src/components/surveys/surveyFormReview.js
+++ client/src/components/surveys/surveyFormReview.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
 import _ from 'lodash';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
-            <div>
+            <div key={name}>
                 <label>{label}</label>
                 <div>
                     {formValues[name]}
@@ -21,17 +22,21 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
         <div>
             <h5>Please confirm your entries</h5>
             {reviewFields}
-            <button className="yellow darken-3 btn-flat"
-                onClick={onCancel}>
+            <button
+                onClick={() => submitSurvey(formValues)}
+                className="yellow darken-3 btn-flat white-text" onClick={onCancel}>
                 Back
+            </button>
+            <button className="green btn-flat right white-text" onClick={onCancel}>
+                Send Survey
+                <i className="material-icons right ">email</i>
             </button>
         </div>
     );
 };
 
 function mapStateToProps(state) {
-    // console.log(state);
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
